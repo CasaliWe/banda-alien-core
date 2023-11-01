54052c9d5068c92b7a3f72198f17b4256cf8bf2c
weslei <wesleicasali18@gmail.com>
2023-11-01 20:42:07 -0300

    att

--- public/js/app.js
+++ public/js/app.js
@@ -87,6 +87,48 @@ const phoneMask = (value) => {
 
 
 
+//ANIMAÇÃO DE DIGITAÇÃO
+var titleAnima = document.querySelector('.anima-digitar')
+var backTexto = document.querySelector('.anima-digitar').textContent
+
+function startAnimaDigitar(){
+
+     var texto = titleAnima.textContent
+
+     var loopRemove = setInterval(() => {
+
+        if(texto.length == 1){
+             clearInterval(loopRemove)
+             segundoAnimaDigitar();
+        }
+         
+        texto = titleAnima.textContent.slice(0, -1)
+        titleAnima.textContent = texto
+        
+     }, 100);
+
+}startAnimaDigitar();
+
+function segundoAnimaDigitar(){
+
+    var arrayLetras = backTexto.split('')
+    
+    arrayLetras.forEach((letra, i)=>{
+          setTimeout(() => {
+            titleAnima.textContent += letra
+            if(i == arrayLetras.length -1){
+                 setTimeout(() => {
+                    startAnimaDigitar();
+                 }, 2000);
+            }
+          }, 100 * i);
+    })
+ 
+}
+
+
+
+
 //HOVER EM CIMA DA CAPA SINGLE PARA MOSTRAR ICONS STRAMS
 document.getElementById("container-capa-single").addEventListener('mouseenter', ()=>{
      document.getElementById("container-icons-stream").style.cssText = 'opacity: 1;'
